3a2f7948ff954ef69b4afb268c773fac07916ec9
Nurazmi <azmimr67@gmail.com>
2022-05-28 19:22:15 +0700

    [ssi_timesheet] 14.0.1.0 * Task 14.0-T005671 * Module baru

---
+++ ssi_timesheet/__manifest__.py
@@ -0,0 +1,35 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Timesheets",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_hr",
+        "ssi_master_data_mixin",
+        "ssi_transaction_open_mixin",
+        "ssi_transaction_confirm_mixin",
+        "ssi_transaction_done_mixin",
+        "ssi_transaction_cancel_mixin",
+        "ssi_duration_mixin",
+        "ssi_employee_document_mixin",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule_data.xml",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_data.xml",
+        "data/policy_template_data.xml",
+        "data/approval_template_data.xml",
+        "menu.xml",
+        "views/hr_timesheet_computation_item_views.xml",
+        "views/hr_timesheet_views.xml",
+    ],
+}
