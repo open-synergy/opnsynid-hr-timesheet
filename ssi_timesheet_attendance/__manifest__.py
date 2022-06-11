e4b578bcef6644cc1f3ca6c298c80ea9601ccf10
Nurazmi <azmimr67@gmail.com>
2022-06-11 22:05:18 +0700

    [ssi_timesheet_attendance] 14.0.1.0.0

---
+++ ssi_timesheet_attendance/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Timesheet Attendance",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_timesheet",
+        "ssi_duration_mixin",
+    ],
+    "data": [
+        "security/res_group_data.xml",
+        "security/ir.model.access.csv",
+        "menu.xml",
+        "views/hr_attendance_reason_views.xml",
+        "views/hr_timesheet_views.xml",
+    ],
+}
