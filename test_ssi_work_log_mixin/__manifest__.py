205c70317894aa2a29bb9c401faba87179ab0446
mikevhe18 <viriyananda.michael@gmail.com>
2022-06-29 17:25:21 +0700

    [test_ssi_work_log_mixin] 14.0.1.0.0

---
+++ test_ssi_work_log_mixin/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Test Module - Work Log Mixin",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "category": "Tools",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_work_log_mixin",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/test_work_log_views.xml",
+    ],
+}
---
+++ test_ssi_work_log_mixin/models/test_work_log.py
@@ -0,0 +1,27 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class TestWorkLogMixin(models.Model):
+    _name = "test.work_log_mixin"
+    _description = "Test Work Log Mixin"
+    _inherit = [
+        "mixin.work_object",
+    ]
+
+    _work_log_create_page = True
+
+    name = fields.Char(
+        string="# Document",
+        default="/",
+    )
+    code = fields.Char(
+        string="Code",
+        default="000",
+    )
+    note = fields.Text(
+        string="Note",
+    )
