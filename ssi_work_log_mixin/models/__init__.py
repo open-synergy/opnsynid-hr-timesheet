227b8ac94476af4d3463c36707af56650e9056e1
mikevhe18 <viriyananda.michael@gmail.com>
2022-06-29 17:24:53 +0700

    [ssi_work_log_mixin] 14.0.1.0.0

---
+++ ssi_work_log_mixin/models/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import (
+    hr_work_log,
+    ir_model,
+    mixin_work_object,
+    hr_timesheet,
+)
