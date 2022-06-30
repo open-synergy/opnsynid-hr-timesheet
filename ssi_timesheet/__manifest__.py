8994e1164054772b323a4d35807c826ad45411cc
Andhitia Rama <andhitia.r@gmail.com>
2022-06-30 08:16:02 +0700

    [ssi_timesheet] 14.0.1.2.0
    * Cascade timesheet computation when delete timesheet

--- ssi_timesheet/__manifest__.py
+++ ssi_timesheet/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Timesheets",
-    "version": "14.0.1.1.0",
+    "version": "14.0.1.2.0",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "LGPL-3",
