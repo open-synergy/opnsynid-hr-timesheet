64eb8db345376b096ede60de8e85ab681aab01d7
mikevhe18 <viriyananda.michael@gmail.com>
2022-09-06 12:29:54 +0700

    [ssi_hr_holiday] 8.0.1.0.1 - Minor Revisions

--- ssi_hr_holiday/__openerp__.py
+++ ssi_hr_holiday/__openerp__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Leave Management",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.0.1",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "LGPL-3",
