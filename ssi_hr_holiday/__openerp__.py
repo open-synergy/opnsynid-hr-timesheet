0934191346e99ccaced22b4341c58e47afed24fa
Andhitia Rama <andhitia.r@gmail.com>
2022-10-18 04:22:09 +0700

    [ssi_hr_holiday] 8.0.1.1.0
    * Fix bugs when restart
    * Required date start and date end on leave
    * Required date start on leave allocation

--- ssi_hr_holiday/__openerp__.py
+++ ssi_hr_holiday/__openerp__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Leave Management",
-    "version": "8.0.1.0.1",
+    "version": "8.0.1.1.0",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "LGPL-3",
