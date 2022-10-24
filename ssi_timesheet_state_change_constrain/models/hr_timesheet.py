3ad903adedffce883fe0a6c8e6fdb8f0028063ca
mikevhe18 <viriyananda.michael@gmail.com>
2022-10-24 18:22:46 +0700

    [ssi_timesheet_state_change_constrain] 14.0.1.0.0

---
+++ ssi_timesheet_state_change_constrain/models/hr_timesheet.py
@@ -0,0 +1,14 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models
+
+
+class HRTimesheet(models.Model):
+    _name = "hr.timesheet"
+    _inherit = [
+        "hr.timesheet",
+        "mixin.state_change_constrain",
+        "mixin.status_check",
+    ]
