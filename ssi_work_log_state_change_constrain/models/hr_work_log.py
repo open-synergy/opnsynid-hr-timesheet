ee18040d53943b20cb23888255a83749072e9d5e
mikevhe18 <viriyananda.michael@gmail.com>
2022-10-24 18:22:17 +0700

    [ssi_work_log_state_change_constrains] 14.0.1.0.0

---
+++ ssi_work_log_state_change_constrain/models/hr_work_log.py
@@ -0,0 +1,14 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models
+
+
+class HRWorkLog(models.Model):
+    _name = "hr.work_log"
+    _inherit = [
+        "hr.work_log",
+        "mixin.state_change_constrain",
+        "mixin.status_check",
+    ]
