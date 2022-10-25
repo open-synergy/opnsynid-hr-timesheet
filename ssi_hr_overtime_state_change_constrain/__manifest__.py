2b4066914f3eea7e90bbac091daced8f3de9ebd7
mikevhe18 <viriyananda.michael@gmail.com>
2022-10-25 07:35:28 +0700

    [ssi_hr_overtime_state_change_constrain] 14.0.1.0.0

---
+++ ssi_hr_overtime_state_change_constrain/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Overtime State Change Constrain",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_hr_overtime",
+        "ssi_state_change_constrain_mixin",
+    ],
+    "data": [],
+}
---
+++ ssi_hr_overtime_state_change_constrain/models/hr_overtime.py
@@ -0,0 +1,16 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models
+
+
+class HROvertime(models.Model):
+    _name = "hr.overtime"
+    _inherit = [
+        "hr.overtime",
+        "mixin.state_change_constrain",
+        "mixin.status_check",
+    ]
+
+    _status_check_create_page = True
