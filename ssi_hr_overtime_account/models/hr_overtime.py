9f7e5b588e794500373e78ca062b013dea941895
mikevhe18 <viriyananda.michael@gmail.com>
2022-10-27 07:34:48 +0700

    [ssi_hr_overtime_account] 14.0.1.0.0

---
+++ ssi_hr_overtime_account/models/hr_overtime.py
@@ -0,0 +1,16 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class HROvertime(models.Model):
+    _inherit = "hr.overtime"
+
+    analytic_account_id = fields.Many2one(
+        string="Analytic Account",
+        comodel_name="account.analytic.account",
+        required=False,
+        ondelete="restrict",
+    )
