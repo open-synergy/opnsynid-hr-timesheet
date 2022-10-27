66fdae16faec1f435171f62e79e0a09807551b32
mikevhe18 <viriyananda.michael@gmail.com>
2022-10-27 07:34:48 +0700

    [ssi_hr_overtime_account] 14.0.1.0.0

---
+++ ssi_hr_overtime_account/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Overtime Account",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_hr_overtime",
+        "ssi_financial_accounting",
+    ],
+    "data": [
+        "views/hr_overtime_views.xml",
+    ],
+}
