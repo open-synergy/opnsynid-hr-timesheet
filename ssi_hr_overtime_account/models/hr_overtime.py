2b3a0483a30ee9c5bc369f541a8dbac53ba51d82
mikevhe18 <viriyananda.michael@gmail.com>
2022-11-22 15:42:16 +0700

    [ssi_hr_overtime_account] 14.0.1.1.0

--- ssi_hr_overtime_account/models/hr_overtime.py
+++ ssi_hr_overtime_account/models/hr_overtime.py
@@ -2,12 +2,38 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+from odoo.tools.safe_eval import safe_eval
 
 
 class HROvertime(models.Model):
     _inherit = "hr.overtime"
 
+    @api.depends(
+        "type_id",
+        "employee_id",
+    )
+    def _compute_allowed_analytic_account_ids(self):
+        for document in self:
+            result = []
+            if document.type_id:
+                type = document.type_id
+                if type.analytic_account_method == "fixed":
+                    if type.analytic_account_ids:
+                        result = type.analytic_account_ids.ids
+                elif type.analytic_account_method == "python":
+                    analytic_account_ids = self._evaluate_analytic_account(type)
+                    if analytic_account_ids:
+                        result = analytic_account_ids
+            document.allowed_analytic_account_ids = result
+
+    allowed_analytic_account_ids = fields.Many2many(
+        string="Allowed Analytic Accounts",
+        comodel_name="account.analytic.account",
+        compute="_compute_allowed_analytic_account_ids",
+        store=False,
+    )
     analytic_account_id = fields.Many2one(
         string="Analytic Account",
         comodel_name="account.analytic.account",
@@ -18,3 +44,30 @@ class HROvertime(models.Model):
             "draft": [("readonly", False)],
         },
     )
+
+    def _get_localdict(self):
+        self.ensure_one()
+        return {
+            "env": self.env,
+            "document": self,
+        }
+
+    def _evaluate_analytic_account(self, type):
+        self.ensure_one()
+        res = False
+        localdict = self._get_localdict()
+        try:
+            safe_eval(type.python_code, localdict, mode="exec", nocopy=True)
+            if "result" in localdict:
+                res = localdict["result"]
+        except Exception as error:
+            msg_err = _("Error evaluating conditions.\n %s") % error
+            raise UserError(msg_err)
+        return res
+
+    @api.onchange(
+        "type_id",
+    )
+    def onchange_analytic_account_id(self):
+        if self.type_id:
+            self.analytic_account_id = False
---
+++ ssi_hr_overtime_account/models/hr_overtime_type.py
@@ -0,0 +1,34 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class HROvertimeType(models.Model):
+    _inherit = "hr.overtime_type"
+
+    analytic_account_method = fields.Selection(
+        string="Analytic Account Selection Method",
+        selection=[
+            ("fixed", "Fixed"),
+            ("python", "Python Code"),
+        ],
+        default="fixed",
+    )
+    analytic_account_ids = fields.Many2many(
+        string="Analytic Accounts",
+        comodel_name="account.analytic.account",
+        relation="rel_overtime_type_2_analytic_account",
+        column1="overtime_type_id",
+        column2="analytic_account_id",
+    )
+    python_code = fields.Text(
+        string="Python Code",
+        default="""# Available variables:
+#  - env: Odoo Environment on which the action is triggered.
+#  - document: record on which the action is triggered; may be void.
+#  - result: Return result, the value is list of Analytic Accounts.
+result = []""",
+        copy=True,
+    )
