be8979275dcccf49109631f96a6734a90215a7ba
Andhitia Rama <andhitia.r@gmail.com>
2022-12-17 16:47:13 +0700

    [ssi_work_log_cost] 14.0.1.0.0

---
+++ ssi_work_log_cost/models/hr_work_log.py
@@ -0,0 +1,157 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+from odoo.tools.safe_eval import safe_eval
+
+
+class HRWorkLog(models.Model):
+    _name = "hr.work_log"
+    _inherit = [
+        "hr.work_log",
+        "mixin.product_line_account",
+    ]
+
+    account_id = fields.Many2one(
+        required=False,
+    )
+    currency_id = fields.Many2one(
+        required=False,
+    )
+    allowed_worklog_product_ids = fields.Many2many(
+        string="Allowed Worklog Products",
+        comodel_name="product.product",
+        compute="_compute_allowed_worklog_product_ids",
+    )
+    allowed_worklog_pricelist_ids = fields.Many2many(
+        string="Allowed Worklog Pricelist",
+        comodel_name="product.pricelist",
+        compute="_compute_allowed_worklog_pricelist_ids",
+    )
+    show_cost_setting_ok = fields.Boolean(
+        string="Show Cost Setting",
+        compute="_compute_policy",
+    )
+
+    @api.depends(
+        "model_id",
+    )
+    def _compute_allowed_worklog_product_ids(self):
+        Product = self.env["product.product"]
+        for document in self:
+            result = []
+            if document.model_id:
+                model = document.model_id
+                if model.work_log_product_selection_method == "fixed":
+                    if model.work_log_product_ids:
+                        result += model.work_log_product_ids.ids
+                    if model.work_log_product_categ_ids:
+                        criteria = [
+                            ("categ_id", "in", model.work_log_product_categ_ids.ids)
+                        ]
+                        result += Product.search(criteria).ids
+                elif model.work_log_product_selection_method == "python":
+                    product_ids = self._evaluate_worklog_product(model)
+                    if product_ids:
+                        result = product_ids
+            document.allowed_worklog_product_ids = result
+
+    @api.depends(
+        "model_id",
+        "currency_id",
+    )
+    def _compute_allowed_worklog_pricelist_ids(self):
+        Pricelist = self.env["product.pricelist"]
+        for document in self:
+            result = []
+            if document.model_id:
+                model = document.model_id
+                if model.work_log_pricelist_selection_method == "fixed":
+                    if model.work_log_pricelist_ids:
+                        result += model.work_log_pricelist_ids.ids
+                elif model.work_log_pricelist_selection_method == "python":
+                    pricelist_ids = self._evaluate_worklog_pricelist(model)
+                    if pricelist_ids:
+                        result = pricelist_ids
+                if len(result) > 0:
+                    criteria = [
+                        ("id", "in", result),
+                        ("currency_id", "=", document.currency_id.id),
+                    ]
+                    result = Pricelist.search(criteria).ids
+            document.allowed_worklog_pricelist_ids = result
+
+    def _compute_policy(self):
+        _super = super(HRWorkLog, self)
+        _super._compute_policy()
+
+    @api.model
+    def _get_policy_field(self):
+        res = super(HRWorkLog, self)._get_policy_field()
+        policy_field = [
+            "show_cost_setting_ok",
+        ]
+        res += policy_field
+        return res
+
+    @api.onchange("product_id")
+    def onchange_name(self):
+        pass
+
+    @api.onchange(
+        "amount",
+    )
+    def onchange_uom_quantity(self):
+        self.uom_quantity = self.amount
+
+    @api.onchange(
+        "allowed_worklog_pricelist_ids",
+        "currency_id",
+    )
+    def onchange_pricelist_id(self):
+        self.pricelist_id = False
+        if self.allowed_worklog_pricelist_ids:
+            self.pricelist_id = self.allowed_worklog_pricelist_ids[0]._origin.id
+
+    @api.onchange(
+        "model_id",
+    )
+    def onchange_usage_id(self):
+        self.usage_id = False
+        if self.model_id:
+            self.usage_id = self.model_id.default_worklog_usage_id
+
+    def _evaluate_worklog_product(self, model):
+        self.ensure_one()
+        res = False
+        localdict = self._get_localdict()
+        try:
+            safe_eval(
+                model.work_log_product_python_code, localdict, mode="exec", nocopy=True
+            )
+            if "result" in localdict:
+                res = localdict["result"]
+        except Exception as error:
+            msg_err = _("Error evaluating conditions.\n %s") % error
+            raise UserError(msg_err)
+        return res
+
+    def _evaluate_worklog_pricelist(self, model):
+        self.ensure_one()
+        res = False
+        localdict = self._get_localdict()
+        try:
+            safe_eval(
+                model.work_log_pricelist_python_code,
+                localdict,
+                mode="exec",
+                nocopy=True,
+            )
+            if "result" in localdict:
+                res = localdict["result"]
+        except Exception as error:
+            msg_err = _("Error evaluating conditions.\n %s") % error
+            raise UserError(msg_err)
+        return res
---
+++ ssi_work_log_cost/models/ir_model.py
@@ -0,0 +1,76 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class IrModel(models.Model):
+    _name = "ir.model"
+    _inherit = "ir.model"
+
+    # Product
+    work_log_product_selection_method = fields.Selection(
+        string="Product Selection Method",
+        selection=[
+            ("fixed", "Fixed"),
+            ("python", "Python Code"),
+        ],
+        default="fixed",
+        required=True,
+    )
+    work_log_product_ids = fields.Many2many(
+        string="Allowed Work Log Products",
+        comodel_name="product.product",
+        relation="rel_model_2_work_log_product",
+        column1="model_id",
+        column2="product_id",
+    )
+    work_log_product_categ_ids = fields.Many2many(
+        string="Allowed Work Log Product Categories",
+        comodel_name="product.category",
+        relation="rel_model_2_work_log_product_category",
+        column1="model_id",
+        column2="categ_id",
+    )
+    work_log_product_python_code = fields.Text(
+        string="Python Code for Work Log Product",
+        default="""# Available variables:
+#  - env: Odoo Environment on which the action is triggered.
+#  - document: record on which the action is triggered; may be void.
+#  - result: Return result, the value is list of Analytic Accounts.
+result = []""",
+        copy=True,
+    )
+    default_worklog_usage_id = fields.Many2one(
+        string="Default Worklog Usage",
+        comodel_name="product.usage_type",
+        ondelete="restrict",
+    )
+
+    # Pricelist
+    work_log_pricelist_selection_method = fields.Selection(
+        string="Pricelist Selection Method",
+        selection=[
+            ("fixed", "Fixed"),
+            ("python", "Python Code"),
+        ],
+        default="fixed",
+        required=True,
+    )
+    work_log_pricelist_ids = fields.Many2many(
+        string="Allowed Work Log Pricelists",
+        comodel_name="product.pricelist",
+        relation="rel_model_2_work_log_pricelist",
+        column1="model_id",
+        column2="pricelist_id",
+    )
+    work_log_pricelist_python_code = fields.Text(
+        string="Python Code for Work Log Pricelist",
+        default="""# Available variables:
+#  - env: Odoo Environment on which the action is triggered.
+#  - document: record on which the action is triggered; may be void.
+#  - result: Return result, the value is list of Analytic Accounts.
+result = []""",
+        copy=True,
+    )
