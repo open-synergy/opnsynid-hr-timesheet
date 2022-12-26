6bed72184febc05d875346b6d46b89c182bd319d
mikevhe18 <viriyananda.michael@gmail.com>
2022-12-26 15:59:14 +0700

    [ssi_hr_leave_allocation_request_batch] 14.0.1.0.0

---
+++ ssi_hr_leave_allocation_request_batch/__manifest__.py
@@ -0,0 +1,28 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Leave Allocation Request Batch",
+    "version": "14.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["ssi_hr_holiday"],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule_data.xml",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_data.xml",
+        "data/policy_template_data.xml",
+        "data/approval_template_data.xml",
+        "views/hr_leave_allocation_request_batch_view.xml",
+        "views/hr_leave_allocation_view.xml",
+    ],
+    "demo": [],
+    "images": [],
+}
