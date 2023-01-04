ddd336760d8f74a56e22139c0d06e51ba28867ca
Andhitia Rama <andhitia.r@gmail.com>
2023-01-04 21:14:26 +0700

    [ssi_timesheet_attendance_work_log] 14.0.1.0.0

---
+++ ssi_timesheet_attendance_work_log/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Timesheet + Attendance + Work Log",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_timesheet_attendance",
+        "ssi_work_log_mixin",
+    ],
+    "data": [
+        "views/hr_timesheet_views.xml",
+    ],
+}
---
+++ ssi_timesheet_attendance_work_log/models/hr_timesheet.py
@@ -0,0 +1,22 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class HRTimesheet(models.Model):
+    _inherit = "hr.timesheet"
+
+    attendance_work_log_diff = fields.Float(
+        string="Attendance - Work Log",
+        compute="_compute_attendance_work_log_diff",
+        store=False,
+    )
+
+    def _compute_attendance_work_log_diff(self):
+        for record in self:
+            result = (
+                record.total_attendance + record.running_attendance
+            ) - record.total_work_log
+            record.attendance_work_log_diff = result
