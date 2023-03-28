f41b220aabd354ce8821cef77ce5dd749584ee9c
usa8bit <usa8bit@gmail.com>
2023-03-28 21:30:10 +0700

    [ssi_holiday_state_change_constrain] 14.0.1.0.0

---
+++ ssi_holiday_state_change_constrain/models/leave_allocation.py
@@ -0,0 +1,16 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models
+
+
+class HrLeaveAllocation(models.Model):
+    _name = "hr.leave_allocation"
+    _inherit = [
+        "hr.leave_allocation",
+        "mixin.state_change_constrain",
+        "mixin.status_check",
+    ]
+
+    _status_check_create_page = True
