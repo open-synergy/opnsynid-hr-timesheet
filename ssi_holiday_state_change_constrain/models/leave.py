c8a814d87e31121e88f7bddb61f02152666734d2
mikevhe18 <viriyananda.michael@gmail.com>
2023-03-29 14:05:03 +0700

    [ssi_holiday_state_change_constrains] 14.0.1.0.0 QC

--- ssi_holiday_state_change_constrain/models/leave.py
+++ ssi_holiday_state_change_constrain/models/leave.py
@@ -7,6 +7,10 @@ from odoo import models
 
 class HRLeave(models.Model):
     _name = "hr.leave"
-    _inherit = ["hr.leave", "mixin.state_change_constrain", "mixin.status_check"]
+    _inherit = [
+        "hr.leave",
+        "mixin.state_change_constrain",
+        "mixin.status_check",
+    ]
 
     _status_check_create_page = True
