a0a76ce262084bd251aea143cc0ed891aced1f22
mikevhe18 <viriyananda.michael@gmail.com>
2023-04-06 16:12:51 +0700

    [ssi_timesheet_attendance] 14.0.1.11.3

--- ssi_timesheet_attendance/__manifest__.py
+++ ssi_timesheet_attendance/__manifest__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Timesheet Attendance",
-    "version": "14.0.1.11.2",
+    "version": "14.0.1.11.3",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "LGPL-3",
