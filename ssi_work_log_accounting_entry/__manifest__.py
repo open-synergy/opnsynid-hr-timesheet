1f277659f225a5e15437d3421acb2a2c5061fb19
usa8bit <usa8bit@gmail.com>
2023-04-06 14:16:46 +0700

    [ssi_work_log_accounting_entry] 14.0.1.0.0

---
+++ ssi_work_log_accounting_entry/__manifest__.py
@@ -0,0 +1,32 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Work Log To Accounting Entry",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_financial_accounting",
+        "ssi_master_data_mixin",
+        "ssi_transaction_confirm_mixin",
+        "ssi_transaction_done_mixin",
+        "ssi_transaction_cancel_mixin",
+        "ssi_work_log_cost",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule_data.xml",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_data.xml",
+        "data/policy_template_data.xml",
+        "data/approval_template_data.xml",
+        "views/work_log_accounting_entry_type_view.xml",
+        "views/work_log_accounting_entry_view.xml",
+    ],
+}
