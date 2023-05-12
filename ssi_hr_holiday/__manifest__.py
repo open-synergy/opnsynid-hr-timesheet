5a1fdf683ded2d3029e25baa1ba264e555a7638e
Miftahussalam <miftah_salam@ymail.com>
2023-05-13 01:26:42 +0700

    [ssi_hr_holiday] 14.0.1.4.0

--- ssi_hr_holiday/__manifest__.py
+++ ssi_hr_holiday/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Leave Management",
-    "version": "14.0.1.3.1",
+    "version": "14.0.1.4.0",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "LGPL-3",
