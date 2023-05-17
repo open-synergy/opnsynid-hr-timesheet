d072550bcf11d8a32dc68adef2688e21e8066a4f
usa8bit <usa8bit@gmail.com>
2023-05-17 13:01:04 +0700

    [ssi_hr_overtime_batch] 14.0.1.0.0

---
+++ ssi_hr_overtime_batch/__manifest__.py
@@ -0,0 +1,29 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Human Resource Overtime Batch",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "category": "Human Resource",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_hr_overtime",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule_data.xml",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_data.xml",
+        "data/policy_template_data.xml",
+        "data/approval_template_data.xml",
+        "views/hr_overtime_batch_views.xml",
+        "views/hr_overtime_views.xml",
+    ],
+    "demo": [],
+}
