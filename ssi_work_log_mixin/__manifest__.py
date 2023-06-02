7a8c7025e9a3f645895694f13be301ca08b7f827
Andhitia Rama <andhitia.r@gmail.com>
2023-06-02 15:34:59 +0700

    Update __manifest__.py

--- ssi_work_log_mixin/__manifest__.py
+++ ssi_work_log_mixin/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Work Log Mixin",
-    "version": "14.0.1.12.1",
+    "version": "14.0.1.13.0",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "LGPL-3",
