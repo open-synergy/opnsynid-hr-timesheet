1390a017a0c113637c9313036e81eb9a95f897b4
mikevhe18 <viriyananda.michael@gmail.com>
2023-07-31 12:27:11 +0700

    [ssi_hr_overtime_state_change_constrain] Publish to Pypi

--- ssi_hr_overtime_state_change_constrain/__manifest__.py
+++ ssi_hr_overtime_state_change_constrain/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Overtime State Change Constrain",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "LGPL-3",
