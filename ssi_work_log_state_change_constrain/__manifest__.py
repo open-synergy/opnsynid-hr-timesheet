ce079f2533903d2401568318f61350a34575d6b4
mikevhe18 <viriyananda.michael@gmail.com>
2023-07-31 12:29:33 +0700

    [ssi_work_log_state_change_constrain] Publish to Pypi

--- ssi_work_log_state_change_constrain/__manifest__.py
+++ ssi_work_log_state_change_constrain/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Work Log State Change Constrain",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.2",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "LGPL-3",
