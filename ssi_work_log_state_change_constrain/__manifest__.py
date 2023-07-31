73fb18daa8984004f90ba4099805461b586cbb3c
mikevhe18 <viriyananda.michael@gmail.com>
2023-07-31 14:50:36 +0700

    [ssi_work_log_state_change_constrain] Change license to AGPL-3

--- ssi_work_log_state_change_constrain/__manifest__.py
+++ ssi_work_log_state_change_constrain/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2022 OpenSynergy Indonesia
 # Copyright 2022 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Work Log State Change Constrain",
     "version": "14.0.1.0.2",
     "website": "https://simetri-sinergi.id",
-    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
-    "license": "LGPL-3",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
     "installable": True,
     "depends": [
         "ssi_work_log_mixin",
