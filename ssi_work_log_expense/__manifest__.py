924e659fa99d3dc9220d1c2d9eff7ed05c08fbfd
mikevhe18 <viriyananda.michael@gmail.com>
2023-07-31 14:44:30 +0700

    [ssi_work_log_expense] Change license to AGPL-3

--- ssi_work_log_expense/__manifest__.py
+++ ssi_work_log_expense/__manifest__.py
@@ -6,8 +6,8 @@
     "name": "Work Log Expense",
     "version": "14.0.1.2.1",
     "website": "https://simetri-sinergi.id",
-    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
-    "license": "LGPL-3",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
     "installable": True,
     "depends": [
         "ssi_financial_accounting",
