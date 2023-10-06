61432819c951acb77095f22c77e7d9d05df6a61a
Miftahussalam <miftah_salam@ymail.com>
2023-10-07 00:08:44 +0700

    [ssi_timesheet_attendance] 14.0.1.12.4

--- ssi_timesheet_attendance/models/hr_timesheet_daily_summary.py
+++ ssi_timesheet_attendance/models/hr_timesheet_daily_summary.py
@@ -9,37 +9,34 @@ from odoo.exceptions import ValidationError
 class HrTimesheetDailySummary(models.Model):
     _inherit = 'hr.timesheet_daily_summary'
 
-    total_hour = fields.Float(
+    total_attendance = fields.Float(
         string="Total Attendance",
         copy=False,
     )
-    total_valid_hour = fields.Float(
+    total_valid_attendance = fields.Float(
         string="Total Valid Attendance",
         copy=False,
     )
 
     def _prepare_daily_summary_vals(self, sheet_id, date):
-        vals = {
-            'sheet_id': sheet_id.id,
-            'date': date,
-        }
-        total_hour = 0
-        total_valid_hour = 0
+        vals = super(HrTimesheetDailySummary, self)._prepare_daily_summary_vals(sheet_id=sheet_id, date=date)
+        total_attendance = 0
+        total_valid_attendance = 0
         attendance_ids = self.env['hr.timesheet_attendance'].search([
             ('sheet_id', '=', sheet_id.id),
             ('date', '=', date),
         ])
         for attendance_id in attendance_ids:
-            total_hour += attendance_id.total_hour
-            total_valid_hour += attendance_id.total_valid_hour
+            total_attendance += attendance_id.total_hour
+            total_valid_attendance += attendance_id.total_valid_hour
         vals.update({
-            'total_hour': total_hour,
-            'total_valid_hour': total_valid_hour,
+            'total_attendance': total_attendance,
+            'total_valid_attendance': total_valid_attendance,
         })
         return vals
 
     def need_to_add(self, vals):
         need_to_add = super(HrTimesheetDailySummary, self).need_to_add(vals=vals)
-        if vals.get('total_hour') or vals.get('total_valid_hour'):
+        if vals.get('total_attendance') or vals.get('total_valid_attendance'):
             need_to_add = True
         return need_to_add
