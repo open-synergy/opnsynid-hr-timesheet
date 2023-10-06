a373602376f10c92eba4586b3a4a2e1544e28923
Miftahussalam <miftah_salam@ymail.com>
2023-10-06 22:36:17 +0700

    [ssi_timesheet] 14.0.1.7.1

--- ssi_timesheet/models/hr_timesheet_daily_summary.py
+++ ssi_timesheet/models/hr_timesheet_daily_summary.py
@@ -2,7 +2,7 @@
 # Copyright 2023 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 
 
