cdb30546cf288b9da9dab7cd610d7a699df65639
mikevhe18 <viriyananda.michael@gmail.com>
2024-10-10 09:44:16 +0700

    [REV] Implementasi decorator

--- ssi_timesheet/__manifest__.py
+++ ssi_timesheet/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2022 OpenSynergy Indonesia
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
+# pylint: disable=C8101
 {
     "name": "Timesheets",
     "version": "14.0.1.9.0",
@@ -18,6 +18,7 @@
         "ssi_transaction_cancel_mixin",
         "ssi_duration_mixin",
         "ssi_employee_document_mixin",
+        "ssi_decorator",
     ],
     "data": [
         "security/ir_module_category_data.xml",
