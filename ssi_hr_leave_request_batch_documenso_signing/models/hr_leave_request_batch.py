cef5145c78f27af26e5d6b51c14eb8c45e908a47
Andhitia Rama <andhitia.r@gmail.com>
2026-03-20 20:19:12 +0700

    [ADD] ssi_hr_leave_request_batch_documenso_signing

---
+++ ssi_hr_leave_request_batch_documenso_signing/models/hr_leave_request_batch.py
@@ -0,0 +1,15 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+
+from odoo import models
+
+
+class HrLeaveRequestBatch(models.Model):
+    _name = "hr.leave_request_batch"
+    _inherit = [
+        "hr.leave_request_batch",
+        "mixin.documenso_signing",
+    ]
+
+    _documenso_signing_create_page = True
