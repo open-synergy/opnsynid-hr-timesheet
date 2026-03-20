f3b284bc7d783211cbe33e29c0e91b4a723d3619
Andhitia Rama <andhitia.r@gmail.com>
2026-03-20 20:20:22 +0700

    [ADD] ssi_hr_overtime_documenso_signing

---
+++ ssi_hr_overtime_documenso_signing/models/hr_overtime.py
@@ -0,0 +1,15 @@
+# Copyright 2024 OpenSynergy Indonesia
+# Copyright 2024 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+
+from odoo import models
+
+
+class HrOvertime(models.Model):
+    _name = "hr.overtime"
+    _inherit = [
+        "hr.overtime",
+        "mixin.documenso_signing",
+    ]
+
+    _documenso_signing_create_page = True
