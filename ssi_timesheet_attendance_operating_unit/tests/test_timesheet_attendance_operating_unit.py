14d3b651b9153e77c11f2e292551ea960f20a947
Andhitia Rama <andhitia.r@gmail.com>
2026-05-16 02:59:13 +0700

    [ADD] Unit test for ssi_timesheet_attendance_operating_unit

---
+++ ssi_timesheet_attendance_operating_unit/tests/test_timesheet_attendance_operating_unit.py
@@ -0,0 +1,13 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo_yaml_test import YamlTransactionCase
+
+from odoo.tests import tagged
+
+
+@tagged("post_install", "-at_install")
+class TestTimesheetAttendanceOperatingUnit(YamlTransactionCase):
+    def test_timesheet_attendance_operating_unit(self):
+        self.run_yaml_scenario("test_data_timesheet_attendance_operating_unit.yaml")
