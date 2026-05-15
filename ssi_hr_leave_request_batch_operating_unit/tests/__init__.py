84eedc3cf60ef323d9cae791cdecc503e859e7d9
Andhitia Rama <andhitia.r@gmail.com>
2026-05-16 02:58:16 +0700

    [ADD] Unit test for ssi_hr_leave_request_batch_operating_unit

---
+++ ssi_hr_leave_request_batch_operating_unit/tests/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Simetri Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import test_hr_leave_request_batch_operating_unit
