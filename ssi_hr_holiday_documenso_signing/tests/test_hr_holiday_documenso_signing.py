3a8155a1fad98c351ee90bd9669a08e62fb98245
Andhitia Rama <andhitia.r@gmail.com>
2026-05-16 02:57:49 +0700

    [ADD] Unit test for ssi_hr_holiday_documenso_signing

---
+++ ssi_hr_holiday_documenso_signing/tests/test_hr_holiday_documenso_signing.py
@@ -0,0 +1,13 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo_yaml_test import YamlTransactionCase
+
+from odoo.tests import tagged
+
+
+@tagged("post_install", "-at_install")
+class TestHrHolidayDocumensoSigning(YamlTransactionCase):
+    def test_hr_holiday_documenso_signing(self):
+        self.run_yaml_scenario("test_data_hr_holiday_documenso_signing.yaml")
