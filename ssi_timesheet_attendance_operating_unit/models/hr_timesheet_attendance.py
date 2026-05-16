1482ca61f3c198db27e83c15eb77e79e1dbb8900
Andhitia Rama <andhitia.r@gmail.com>
2026-05-16 08:43:25 +0700

    [UPD] ssi_timesheet_attendance_operating_unit
    * Add onchange_operating_unit_id on sheet_id change
    * Add onchange test scenario for operating unit

--- ssi_timesheet_attendance_operating_unit/models/hr_timesheet_attendance.py
+++ ssi_timesheet_attendance_operating_unit/models/hr_timesheet_attendance.py
@@ -2,7 +2,7 @@
 # Copyright 2026 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import api, models
 
 
 class HRTimesheetAttendance(models.Model):
@@ -11,3 +11,8 @@ class HRTimesheetAttendance(models.Model):
         "hr.timesheet_attendance",
         "mixin.single_operating_unit",
     ]
+
+    @api.onchange("sheet_id")
+    def onchange_operating_unit_id(self):
+        if self.sheet_id:
+            self.operating_unit_id = self.sheet_id.operating_unit_id
